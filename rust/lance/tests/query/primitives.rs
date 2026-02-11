154d819f06a1fa91d947e9bbd16d9329ab385883
fenfeng9 <fenfeng9@qq.com>
2026-02-12 04:09:57 +0800

    fix: correct OR null handling for BlockList|BlockList

--- rust/lance/tests/query/primitives.rs
+++ rust/lance/tests/query/primitives.rs
@@ -81,6 +81,7 @@ async fn test_query_integer(#[case] data_type: DataType) {
             test_filter(&original, &ds, "value is not null").await;
             test_filter(&original, &ds, "(value != 0) OR (value < 20)").await;
             test_filter(&original, &ds, "NOT ((value != 0) OR (value < 20))").await;
+            test_filter(&original, &ds, "(c1 != 71) OR ((c1 != 52) OR (c1 IS NULL))").await;
         })
         .await
 }
